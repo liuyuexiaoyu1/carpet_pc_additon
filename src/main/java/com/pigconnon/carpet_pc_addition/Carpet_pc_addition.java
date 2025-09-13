482ae7167dbe080c45d598610e45a11182ccf407
小雨 六月 <liuyuexiaoyu1@outlook.com>
2025-09-13 10:46:49 +0800

    Initial commit

---
+++ src/main/java/com/pigconnon/carpet_pc_addition/Carpet_pc_addition.java
@@ -0,0 +1,10 @@
+package com.pigconnon.carpet_pc_addition;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Carpet_pc_addition implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
